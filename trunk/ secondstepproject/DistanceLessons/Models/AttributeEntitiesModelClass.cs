3e993ae6e7197157b047aaa7c80f06544779b0d9
freem321@gmail.com <freem321@gmail.com@c0119bda-24cf-95a5-26e3-074bea0b03a2>
2011-10-08 18:31:53 +0000

    Перший тестовий коміт. Готова модель бази даних .

---
+++ trunk/ secondstepproject/DistanceLessons/Models/AttributeEntitiesModelClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//for attributes
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Distance_Lesson.Models
+{
+    [MetadataType(typeof(LoginMetaData))]
+    public partial class Login
+    {
+    }
+
+    public class LoginMetaData
+    {
+        [Required]
+        [DisplayName("Логін")]
+        public string Login { get; set; }
+
+        [Required]
+        [DisplayName("Пароль")]
+        public string Password { get; set; }
+            
+        //etc...
+        //...
+    } 
+}
---
+++ trunk/ secondstepproject/DistanceLessons/Models/UserRepository.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Security.Cryptography;
+
+namespace Distance_Lesson.Models
+{
+    public class UserRepository
+    {
+
+        private dbEntities _db;
+        public dbEntities db
+        {
+            get
+            {
+                if (_db == null) _db = new dbEntities();
+                return _db;
+            }
+        }
+
+
+        private static string CreateSalt()
+        {
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] buff = new byte[32];
+            rng.GetBytes(buff);
+
+            return Convert.ToBase64String(buff);
+        }
+
+        private static string CreatePasswordHash(string pwd, string salt)
+        {
+            string saltAndPwd = String.Concat(pwd, salt);
+            string hashedPwd =
+                    FormsAuthentication.HashPasswordForStoringInConfigFile(
+                    saltAndPwd, "md5");
+            return hashedPwd;
+        }
+
+        public MembershipUser CreateUser(string username, string password, string email)
+        {
+            using (dbEntities db = new dbEntities())
+            {
+                User user = new User();
+
+                username = username.Trim();
+                password = password.Trim();
+                email = email.Trim();
+
+                user.Login = username;
+                user.Email = email;
+                user.Role = 1;
+                user.PasswordSalt = CreateSalt();
+                user.CreatedData = DateTime.Now;
+                user.IsActived = true;
+                user.IsLockedOut = false;
+                user.LastLoginData = DateTime.Now;
+                user.Password = CreatePasswordHash(password, user.PasswordSalt);
+                db.AddToUsers(user);
+                db.SaveChanges();
+
+                return GetUser(username);
+            }
+        }
+
+        public string GetUserNameByEmail(string email)
+        {
+            
+                var result = from u in db.Users where (u.Email == email) select u;
+
+                if (result.Count() != 0)
+                {
+                    var dbuser = result.FirstOrDefault();
+
+                    return dbuser.Login;
+                }
+                else
+                {
+                    return "";
+                }
+            
+        }
+
+        public MembershipUser GetUser(string username)
+        {
+                var result = from u in db.Users where (u.Login == username) select u;
+
+                if (result.Count() != 0)
+                {
+                    var dbuser = result.FirstOrDefault();
+
+                    string _username = dbuser.Login;
+                    Guid _providerUserKey = dbuser.UserId;
+                    string _email = dbuser.Email;
+                    int _permissions = dbuser.Role;
+                    string _passwordQuestion = "";
+                    string _comment = "";
+                    bool _isApproved = dbuser.IsActived;
+                    bool _isLockedOut = dbuser.IsLockedOut;
+                    DateTime _creationDate = dbuser.CreatedData;
+                    DateTime _lastLoginDate = dbuser.LastLoginData;
+                    DateTime _lastActivityDate = DateTime.Now;
+                    DateTime _lastPasswordChangedDate = DateTime.Now;
+                    DateTime _lastLockedOutDate = DateTime.Now;
+
+                    MembershipUser user = new MembershipUser("CustomMembershipProvider",
+                                              _username,
+                                              _providerUserKey,
+                                              _email,
+                                              _passwordQuestion,
+                                              _comment,
+                                              _isApproved,
+                                              _isLockedOut,
+                                              _creationDate,
+                                              _lastLoginDate,
+                                              _lastActivityDate,
+                                              _lastPasswordChangedDate,
+                                              _lastLockedOutDate);
+
+                    return user;
+                }
+                else
+                {
+                    return null;
+                }
+            
+        }
+
+        public bool ValidateUser(string username, string password)
+        {
+            var dbuser = db.Users.FirstOrDefault(x => x.Login == username);
+            return dbuser != null && dbuser.Password == CreatePasswordHash(password, dbuser.PasswordSalt);
+        }
+    }
+}
