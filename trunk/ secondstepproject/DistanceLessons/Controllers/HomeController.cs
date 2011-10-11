3526f939055bb0d4e45727019e483043203290fc
AKatholyk.VK@gmail.com <AKatholyk.VK@gmail.com@c0119bda-24cf-95a5-26e3-074bea0b03a2>
2011-10-11 06:44:16 +0000

    Виведення напрямків навчання, курсів та викладчів - інформація з БД яку може бачити анонім.

--- trunk/ secondstepproject/DistanceLessons/Controllers/HomeController.cs
+++ trunk/ secondstepproject/DistanceLessons/Controllers/HomeController.cs
@@ -31,5 +31,38 @@ namespace DistanceLessons.Controllers
 
             return View();
         }
+
+        public ActionResult Teachers()
+        {
+            DataEntitiesManager dataManager = new DataEntitiesManager();
+            ViewBag.Teachers = dataManager.QTeachers();
+
+            return View();
+        }
+
+        public ActionResult Categories()
+        {
+            DataEntitiesManager dataManager = new DataEntitiesManager();
+            ViewBag.Categories = dataManager.QCategorys();
+
+            return View();
+        }
+
+        public ActionResult Courses(DataEntitiesManager.RQCategorys cat)
+        {
+            DataEntitiesManager dataManager = new DataEntitiesManager();
+            List<DataEntitiesManager.RQCourses> lst = new List<DataEntitiesManager.RQCourses>();
+
+            var courseLst = dataManager.QCourses();
+
+            foreach (DataEntitiesManager.RQCourses obj in courseLst)
+            {
+                if (obj.category == cat.title)
+                lst.Add(obj);
+            }
+
+            ViewBag.Courses = lst;
+            return View();
+        }
     }
 }
