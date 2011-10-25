35ae6ff109f458cabbeea10ed1b614e4538e77d6
a.titanka@gmail.com <a.titanka@gmail.com@c0119bda-24cf-95a5-26e3-074bea0b03a2>
2011-10-25 17:59:52 +0000

    частково змінені таблиці з організації лекцій, добавлені можливості створення модулів і лекцій

---  secondstepproject/DistanceLessons/Models/DataEntitiesManager/Lessons.cs
+++  secondstepproject/DistanceLessons/Models/DataEntitiesManager/Lessons.cs
@@ -12,31 +12,130 @@ namespace DistanceLessons.Models
             return _db.Lessons.ToList<Lesson>();
         }
 
-        public List<Lesson> UserLessons(string username)
+      /*  public List<Lesson> UserLessons(string username)
         {
             Guid userId = GetUserId(username);
             return (from lessons in _db.Lessons
                    where lessons.UserId == userId
                    select lessons).ToList();
         }
+        */
+        public Lesson GetLessonByID(Guid id)
+        {
+            return _db.Lessons.SingleOrDefault(c => c.LessonId == id);
+        }
+
+
+        public void DeleteLesson(Guid id)
+        {
+            var cat = _db.Lessons.SingleOrDefault(c => c.LessonId == id);
+            _db.DeleteObject(cat);
+            Save();
+        }
+
+        public void AddLesson(Lesson obj)
+        {
+            _db.Lessons.AddObject(obj);
+            Save();
+        }
 
         public List<RQLessons> QLessons(string Course)
         {
             var Query =
                 (
-                from l in GetLessonList()
-                from c in GetCourseList()
-                where l.CourseId == c.CourseId && c.Title == Course
-                orderby l.Title
-                select new RQLessons
-                {
-                    id = l.LessonId,
-                    title = l.Title,
-                    text = l.Text
-                }
+                  from l in GetLessonList()
+                  from c in GetCourseList()
+                  from m in GetModuleList()
+                  where c.Title == Course && m.CourseId == c.CategoryId && l.ModuleId == m.ModuleId
+                  orderby l.Title
+                  select new RQLessons
+                  {
+                      id = l.LessonId,
+                      title = l.Title,
+                      text = l.Text,
+                      module = m.Title,
+                      descripton = l.Description
+                  }
                 ).ToList<RQLessons>();
 
             List<RQLessons> lst = new List<RQLessons>();
+            foreach (var i in Query)
+                lst.Add(i);
+
+            return lst;
+        }
+
+        public List<RQLessons> GetLessonsByCourse(string Course)
+        {
+            var Query =
+                (
+                  from l in GetLessonList()
+                  from c in GetCourseList()
+                  from m in GetModuleList()
+                  where c.Title == Course && m.CourseId == c.CategoryId && l.ModuleId == m.ModuleId
+                  orderby l.Title
+                  select new RQLessons
+                  {
+                      id = l.LessonId,
+                      title = l.Title,
+                      text = l.Text,
+                      module = m.Title,
+                      descripton = l.Description
+                  }
+                ).ToList<RQLessons>();
+
+            List<RQLessons> lst = new List<RQLessons>();
+            foreach (var i in Query)
+                lst.Add(i);
+
+            return lst;
+        }
+
+        public List<Lesson> GetLessonsByModule(Guid id_mod)
+        {
+            var Query =
+                (
+                from l in GetLessonList()
+                where l.ModuleId == id_mod
+                orderby l.Title
+                select new Lesson
+                {
+                    LessonId = l.LessonId,
+                    Title = l.Title,
+                    Text = l.Text,
+                    Description = l.Description
+                }
+                ).ToList<Lesson>();
+
+            List<Lesson> lst = new List<Lesson>();
+            foreach (var i in Query)
+                lst.Add(i);
+
+            return lst;
+        }
+
+        public List<Lesson> GetLessonsByTeacherId(Guid UserId)
+        {
+            var Query =
+                (
+                from l in GetLessonList()
+                from c in GetCourseList()
+                from m in GetModuleList()
+                where c.UserId == UserId && m.CourseId == c.CategoryId && l.ModuleId == m.ModuleId
+                orderby l.ModuleId
+                select new Lesson
+                {
+                    LessonId = l.LessonId,
+                    Title = l.Title,
+                    Publication = l.Publication,
+                    ModuleId = l.ModuleId,
+                    Text = l.Text,
+                    Description = l.Description
+                }
+
+                ).ToList<Lesson>();
+
+            List<Lesson> lst = new List<Lesson>();
             foreach (var i in Query)
                 lst.Add(i);
 
---  secondstepproject/DistanceLessons/Models/DataEntitiesManager/Modules.cs
+++  secondstepproject/DistanceLessons/Models/DataEntitiesManager/Modules.cs
@@ -11,5 +11,75 @@ namespace DistanceLessons.Models
         {
             return _db.Modules.ToList<Module>();
         }
+        public Module GetModulesByID(Guid id)
+        {
+            return _db.Modules.SingleOrDefault(c => c.ModuleId == id);
+        }
+
+
+        public void DeleteModule(Guid id)
+        {
+            var cat = _db.Modules.SingleOrDefault(c => c.ModuleId == id);
+            _db.DeleteObject(cat);
+            Save();
+        }
+
+        public void AddModule(Module obj)
+        {
+            _db.Modules.AddObject(obj);
+            Save();
+        }
+
+
+        public List<RQModules> GetModulesByCourseId(Guid courseId)
+        {
+            var Query =
+                (
+                from m in GetModuleList()
+                from c in GetCourseList()
+                where m.CourseId == c.CourseId && c.CourseId == courseId
+                orderby m.Title
+                select new RQModules
+                {
+                    id = m.ModuleId,
+                    title = m.Title,
+                    course = c.Title
+                }
+                ).ToList<RQModules>();
+
+            List<RQModules> lst = new List<RQModules>();
+            foreach (var i in Query)
+                lst.Add(i);
+
+            return lst;
+        }
+        /*
+                public List<Lesson> GetModulesByTeacherId(Guid UserId)
+                {
+                    var Query =
+                        (
+                        from m in GetModuleList()
+                        from c in GetCourseList()
+                        where c.UserId == UserId && m.CourseId == c.CourseId
+                        orderby l.CourseId
+                        select new Lesson
+                        {
+                            LessonId = l.LessonId,
+                            Title = l.Title,
+                            Publication = l.Publication,
+                            CourseId = l.CourseId,
+                            Text = l.Text,
+                            UserId = l.UserId
+                        }
+
+                        ).ToList<Lesson>();
+
+                    List<Lesson> lst = new List<Lesson>();
+                    foreach (var i in Query)
+                        lst.Add(i);
+
+                    return lst;
+
+                }*/
     }
 }
