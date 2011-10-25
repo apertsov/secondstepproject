51187e773baaad75cfa3fb95d55f8b7131748c96
Psy.ak47@gmail.com <Psy.ak47@gmail.com@c0119bda-24cf-95a5-26e3-074bea0b03a2>
2011-10-25 11:58:40 +0000

    add VirtualJournalController.cs create class RQUserModules

---  secondstepproject/DistanceLessons/Models/DataEntitiesManager/UserModules.cs
+++  secondstepproject/DistanceLessons/Models/DataEntitiesManager/UserModules.cs
@@ -11,5 +11,41 @@ namespace DistanceLessons.Models
         {
             return _db.UserModules.ToList<UserModule>();
         }
+
+        public List<RQUserModules> GetUserProgress()
+        {
+
+            var Query =
+                (
+                  
+                    from ui in GetInfoList()
+                    group ui by new { ui.LastName, ui.FirstName, ui.UserId} into usrinf
+                    from um in GetUserModuleList()
+                    from m in GetModuleList()
+                    from cou in GetCourseList()
+                    where um.UserId == usrinf.Key.UserId && cou.CourseId == m.CourseId && um.ModuleId == m.ModuleId
+
+                    orderby usrinf.Key.LastName
+                    select new RQUserModules
+                    {
+                        id = um.UserModuleId,
+                        firstname = usrinf.Key.FirstName,
+                        lastname = usrinf.Key.LastName,
+                        course = cou.Title,
+                        module = m.Title,
+                        progress = um.Passed
+                    }
+
+
+
+
+        ).ToList<RQUserModules>();
+
+            List<RQUserModules> lst = new List<RQUserModules>();
+            foreach (var i in Query)
+                lst.Add(i);
+
+            return lst;
+        }
     }
 }
