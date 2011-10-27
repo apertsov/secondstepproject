01ae73666f40ddcd033e31340b8475bb254e25c4
ammi.ua@gmail.com <ammi.ua@gmail.com@c0119bda-24cf-95a5-26e3-074bea0b03a2>
2011-10-27 17:25:30 +0000

    +Контроллер новин. +Аяксу

---
+++ trunk/ secondstepproject/DistanceLessons/Controllers/NewsController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DistanceLessons.Models;
+
+namespace DistanceLessons.Controllers
+{
+    public class NewsController : Controller
+    {
+
+        private int itemOnPage = 5;
+        private DataEntitiesManager _db = new DataEntitiesManager();
+
+        //
+        // GET: /News/
+
+        public ActionResult Index(int numPage = 0)
+        {
+            ViewData["numPage"] = numPage;
+            ViewData["itemsCount"] = _db.GetNewsList().Count;
+            ViewData["itemOnPage"] = itemOnPage;
+
+            if (!Request.IsAjaxRequest())
+            {
+                return View(_db.GetNewsList_time(itemOnPage, numPage));
+            }
+            else
+            {
+                return PartialView("_New_PartialPage", _db.GetNewsList_time(itemOnPage, numPage));
+            }
+        }
+
+        [HttpGet]
+        public ActionResult CreateNew()
+        {
+            if (!Request.IsAjaxRequest())
+            {
+                return View();
+            }
+            else
+            {
+                return PartialView("_CreateNew_PartialPage");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CreateNew(News obj)
+        {
+            try
+            {
+                obj.NewId = Guid.NewGuid();
+                obj.Publication = System.DateTime.Now;
+                obj.UserId = _db.GetUserId(User.Identity.Name);
+
+                _db.AddNew(obj);
+                _db.Save();
+            }
+            catch { }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult EditNew(Guid id)
+        {
+            if (!Request.IsAjaxRequest())
+            {
+                return View(_db.GetNew(id));
+            }
+            else
+            {
+                return PartialView("_EditNew_PartialPage", _db.GetNew(id));
+            }
+        }
+
+        [HttpPost]
+        public ActionResult EditNew(News obj)
+        {
+            News old = _db.GetNew(obj.NewId);
+            UpdateModel(old);
+            _db.Save();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult DeleteNew(Guid id)
+        {
+            _db.DeleteNew(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
