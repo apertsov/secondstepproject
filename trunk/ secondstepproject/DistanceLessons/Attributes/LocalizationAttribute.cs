4c4dd31bc7e0520c6da8de94f502caf274c16e5f
ammi.ua@gmail.com <ammi.ua@gmail.com@c0119bda-24cf-95a5-26e3-074bea0b03a2>
2011-11-17 20:51:39 +0000

    Globalization & Localization. First Step.

---
+++ trunk/ secondstepproject/DistanceLessons/Attributes/LocalizationAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DistanceLessons.Attributes
+{
+    public class LocalizationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            string culture = (filterContext.HttpContext.Session["culture"] != null)
+                                 ? filterContext.HttpContext.Session["culture"].ToString()
+                                 : "uk-ua";
+
+            CultureInfo cultureInfo = new CultureInfo(culture);
+
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+        }
+    }
+}
---
+++ trunk/ secondstepproject/DistanceLessons/Helpers/Localization.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Compilation;
+using System.Web.Mvc;
+
+namespace DistanceLessons.Helpers
+{
+    public static class Localization
+    {
+        public static string Resource(this HtmlHelper helper, string expr, params object[] args)
+        {
+            string path = ((RazorView) helper.ViewContext.View).ViewPath;
+
+            ResourceExpressionFields fields =
+                (ResourceExpressionFields)
+                (new ResourceExpressionBuilder()).ParseExpression(expr, typeof (string),
+                                                                  new ExpressionBuilderContext(path));
+            return (!string.IsNullOrEmpty(fields.ClassKey))
+                       ? string.Format(
+                           (string)
+                           helper.ViewContext.HttpContext.GetGlobalResourceObject(fields.ClassKey, fields.ResourceKey,
+                                                                                  CultureInfo.CurrentUICulture), args)
+                       : string.Format(
+                           (string)
+                           helper.ViewContext.HttpContext.GetLocalResourceObject(path, fields.ResourceKey,
+                                                                                 CultureInfo.CurrentUICulture), args);
+        }
+    }
+}
