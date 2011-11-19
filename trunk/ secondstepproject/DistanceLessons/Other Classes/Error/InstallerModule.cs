7dfb646198f17aacea1fb0b34ac28377df831570
ammi.ua@gmail.com <ammi.ua@gmail.com@c0119bda-24cf-95a5-26e3-074bea0b03a2>
2011-11-19 16:29:23 +0000

    add: profile(view+action) - detail add: NotFoundPage

---
+++ trunk/ secondstepproject/DistanceLessons/Other Classes/Error/InstallerModule.cs
@@ -0,0 +1,65 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace NotFoundMvc
+{
+    class InstallerModule : IHttpModule
+    {
+        static bool installed;
+        static object installerLock = new object();
+
+        public void Init(HttpApplication application)
+        {
+            if (!installed)
+            {
+                lock (installerLock)
+                {
+                    if (!installed)
+                    {
+                        Install();
+                        installed = true;
+                    }
+                }
+            }
+        }
+
+        void Install()
+        {
+            WrapControllerBuilder();
+            AddNotFoundRoute();
+            AddCatchAllRoute();
+        }
+
+        void WrapControllerBuilder()
+        {
+            ControllerBuilder.Current.SetControllerFactory(
+                new ControllerFactoryWrapper(
+                    ControllerBuilder.Current.GetControllerFactory()
+                )
+            );
+        }
+
+        void AddNotFoundRoute()
+        {
+            // To allow IIS to execute "/notfound" when requesting something which is disallowed,
+            // such as /bin or /add_data.
+            RouteTable.Routes.MapRoute(
+                "NotFound",
+                "notfound",
+                new { controller = "NotFound", action = "NotFound" }
+            );
+        }
+
+        void AddCatchAllRoute()
+        {
+            RouteTable.Routes.MapRoute(
+                "NotFound-Catch-All",
+                "{*any}",
+                new { controller = "NotFound", action = "NotFound" }
+            );
+        }
+
+        public void Dispose() { }
+    }
+}
