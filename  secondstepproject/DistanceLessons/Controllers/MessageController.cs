9a0b3c44a90b50be3cf1a9e73cfedc42b1924f19
AKatholyk.VK@gmail.com <AKatholyk.VK@gmail.com@c0119bda-24cf-95a5-26e3-074bea0b03a2>
2011-11-26 18:57:42 +0000

    Add: reading messages, sending request, working with status

---  secondstepproject/DistanceLessons/Controllers/MessageController.cs
+++  secondstepproject/DistanceLessons/Controllers/MessageController.cs
@@ -37,15 +37,7 @@ namespace DistanceLessons.Controllers
             return View(dm.GetMessageList_UserID(_db.GetUser(_db.GetUserId(User.Identity.Name)).Login, 1));
         }
 
-        //
-        // GET: /Message/Details/5
-
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        
+                
         [HttpGet]
         public ActionResult Create()
         {
@@ -142,28 +134,86 @@ namespace DistanceLessons.Controllers
             return RedirectToAction("Index");
 
         }
- 
-        public ActionResult Edit(int id)
+
+        [HttpGet]
+        public ActionResult Edit(Guid id)
         {
-            return View();
+            List<Message> msgList = _db.GetMessageList();
+            
+
+            foreach (Message msg in msgList)
+            {
+                if (msg.MessageId == id)
+                {
+                    msg.User = _db.GetUser(msg.UserId_From);
+                    msg.User1 = _db.GetUser(msg.UserId_To);
+                    //msg.Status = 0;
+                    _db.UpdateMessageStatus(msg);
+
+                    return View(msg);
+                    //break;
+                }
+            }
+
+            Message msgEr = new Message();
+
+            msgEr.MessageId = id;
+            msgEr.Message1 = "You have no such message";
+
+            return View(msgEr);
         }
 
-        //
-        // POST: /Message/Edit/5
+        [HttpGet]
+        public ActionResult CreateWithMessage(string message)
+        {
+            Message msg = new Message();
+            msg.DateOfSender = System.DateTime.Now;
+            msg.Message1 = message;
+
+            string logIn = Request.Form["Login"];
+
+            return View("Create",msg);
+        }
 
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult CreateWithMessage(Message obj)
         {
-            try
+            string logIn = Request.Form["Login"];
+
+            List<User> userList = _db.GetUserList();
+
+            foreach (User userObj in userList)
             {
-                // TODO: Add update logic here
- 
-                return RedirectToAction("Index");
+                if (userObj.Login == logIn)
+                {
+                    obj.UserId_From = _db.GetUser(User.Identity.Name).UserId;
+                    obj.UserId_To = _db.GetUserId(logIn);
+
+                    if (obj.UserId_From != obj.UserId_To)
+                    {
+                        obj.MessageId = Guid.NewGuid();
+                        obj.DateOfSender = System.DateTime.Now;
+                        obj.Status = 1;
+                        _db.AddMessage(obj);
+                        _db.Save();
+
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        //ViewBag.ErrorUser = 1;
+                        TempData["Message"] = "Ви знаходитесь під цим логіном";
+                        return RedirectToAction("Create");
+                    }
+                    // break;
+                }
+
             }
-            catch
-            {
-                return View();
-            }
+
+            //ViewBag.ErrorUser = 0;
+            TempData["Message"] = "Користувач з таким логіном відсутній";
+            return RedirectToAction("CreateWithMessage");
+
         }
 
     [HttpGet]
