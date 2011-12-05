5185c191383c716f967c0071c15530a5178e7723
ammi.ua@gmail.com <ammi.ua@gmail.com@c0119bda-24cf-95a5-26e3-074bea0b03a2>
2011-12-05 19:36:32 +0000

    repair some error

---  secondstepproject/DistanceLessons/Controllers/EducationController.cs
+++  secondstepproject/DistanceLessons/Controllers/EducationController.cs
@@ -9,7 +9,7 @@ namespace DistanceLessons.Controllers
     public class EducationController : Controller
     {
         private int itemOnPage = 10;
-        private DataEntitiesManager _db = new DataEntitiesManager();
+        private readonly DataEntitiesManager _db = new DataEntitiesManager();
 
         //
         // GET: /Education/
@@ -35,27 +35,30 @@ namespace DistanceLessons.Controllers
             Session["CategoryId"] = id;
 
             Session["page"] = 0;
-            Session["itemsCount"] = _db.GetCourseByCategory_ListCount((Guid)Session["CategoryId"], 0).Count;
+            Session["itemsCount"] = _db.GetCourseByCategory_ListCount((Guid) Session["CategoryId"], 0).Count;
             Session["itemOnPage"] = itemOnPage;
             Session["withStatus"] = 0;
 
-            return PartialView("_CoursesList", _db.GetUserCourse((Guid)Session["CategoryId"], _db.GetUserId(User.Identity.Name), 0, itemOnPage, 0));
+            return PartialView("_CoursesList",
+                               _db.GetUserCourse((Guid) Session["CategoryId"], _db.GetUserId(User.Identity.Name), 0,
+                                                 itemOnPage, 0));
         }
 
         public ActionResult courses(int withStatus, int numPage)
         {
             Session["page"] = numPage;
-            Session["itemsCount"] = _db.GetCourseByCategory_ListCount((Guid)Session["CategoryId"], withStatus).Count;
+            Session["itemsCount"] = _db.GetCourseByCategory_ListCount((Guid) Session["CategoryId"], withStatus).Count;
             Session["itemOnPage"] = itemOnPage;
             Session["withStatus"] = withStatus;
 
             return PartialView("_Courses",
-                               _db.GetUserCourse((Guid)Session["CategoryId"], _db.GetUserId(User.Identity.Name), withStatus, itemOnPage, numPage));
+                               _db.GetUserCourse((Guid) Session["CategoryId"], _db.GetUserId(User.Identity.Name),
+                                                 withStatus, itemOnPage, numPage));
         }
 
         public ActionResult SubscribeOnCourse(Guid CourseId)
         {
-            UserCours subscribe = new UserCours();
+            var subscribe = new UserCours();
             subscribe.UserCourseId = Guid.NewGuid();
             subscribe.CourseId = CourseId;
             subscribe.UserId = _db.GetUserId(User.Identity.Name);
@@ -63,21 +66,30 @@ namespace DistanceLessons.Controllers
             _db.Save();
 
             return PartialView("_Courses",
-                               _db.GetUserCourse((Guid)Session["CategoryId"], _db.GetUserId(User.Identity.Name), (int)Session["withStatus"], itemOnPage, (int)Session["page"]));
+                               _db.GetUserCourse((Guid) Session["CategoryId"], _db.GetUserId(User.Identity.Name),
+                                                 (int) Session["withStatus"], itemOnPage, (int) Session["page"]));
         }
 
         public ActionResult DeleteSubscribeOnCourse(Guid CourseId)
         {
             _db.DeleteUserCourse(_db.GetUserCourseId(_db.GetUserId(User.Identity.Name), CourseId));
             return PartialView("_Courses",
-                               _db.GetUserCourse((Guid)Session["CategoryId"], _db.GetUserId(User.Identity.Name), (int)Session["withStatus"], itemOnPage, (int)Session["page"]));
+                               _db.GetUserCourse((Guid) Session["CategoryId"], _db.GetUserId(User.Identity.Name),
+                                                 (int) Session["withStatus"], itemOnPage, (int) Session["page"]));
         }
 
         public ActionResult ConfirmSubscribeOnCourse(Guid CourseId, Guid TeacherId)
         {
-            Message confirm = new Message();
+            var confirm = new Message();
             confirm.MessageId = Guid.NewGuid();
-            confirm.Message1 = "Користувач <a href=profile/info?user=" + User.Identity.Name + ">" + _db.GetUser(User.Identity.Name).Login + "</a> подав заявку на підписання на Ваш курс - " + _db.GetCourse(CourseId).Title + ".&nbsp;<a href=\"/profile/confirmsubscribe?CourId=" + CourseId + "&amp;SubscribeUser=" + _db.GetUser(User.Identity.Name).UserId + "&amp;MessId=" + confirm.MessageId + "\">Затверджую</a>&nbsp;<a href=\"/profile/cancelsubscribe?CourId=" + CourseId + "&amp;SubscribeUser=" + _db.GetUser(User.Identity.Name).UserId + "&amp;MessId=" + confirm.MessageId + "\">Відмовляю</a>";
+            confirm.Message1 = "Користувач <a href=profile/info?user=" + User.Identity.Name + ">" +
+                               _db.GetUser(User.Identity.Name).Login + "</a> подав заявку на підписання на Ваш курс - " +
+                               _db.GetCourse(CourseId).Title + ".&nbsp;<a href=\"/profile/confirmsubscribe?CourId=" +
+                               CourseId + "&amp;SubscribeUser=" + _db.GetUser(User.Identity.Name).UserId +
+                               "&amp;MessId=" + confirm.MessageId +
+                               "\">Затверджую</a>&nbsp;<a href=\"/profile/cancelsubscribe?CourId=" + CourseId +
+                               "&amp;SubscribeUser=" + _db.GetUser(User.Identity.Name).UserId + "&amp;MessId=" +
+                               confirm.MessageId + "\">Відмовляю</a>";
             confirm.DateOfSender = DateTime.Now;
             confirm.UserId_From = _db.GetUserId(User.Identity.Name);
             confirm.UserId_To = TeacherId;
@@ -88,12 +100,13 @@ namespace DistanceLessons.Controllers
             _db.AddMessage_status(_db.GetMessageStatus(confirm, _db.GetUser(confirm.UserId_From).Login), 2);
 
             return PartialView("_Courses",
-                               _db.GetUserCourse((Guid)Session["CategoryId"], _db.GetUserId(User.Identity.Name), (int)Session["withStatus"], itemOnPage, (int)Session["page"]));
+                               _db.GetUserCourse((Guid) Session["CategoryId"], _db.GetUserId(User.Identity.Name),
+                                                 (int) Session["withStatus"], itemOnPage, (int) Session["page"]));
         }
 
         public ActionResult UserSubscribs()
         {
-            UserCourseAndCategoriesModel temp = new UserCourseAndCategoriesModel();
+            var temp = new UserCourseAndCategoriesModel();
             temp.UserCourses = _db.GetUserCourseListByUser(_db.GetUserId(User.Identity.Name));
             temp.Categories = _db.GetCategoriesList(temp.UserCourses);
             temp.CoursesLessons = _db.GetLessonsListByCoursesList(temp.UserCourses);
