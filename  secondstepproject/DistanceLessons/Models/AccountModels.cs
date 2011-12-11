298ba860d00cb7f216b6b3041f245f8279e1df21
ammi.ua@gmail.com <ammi.ua@gmail.com@c0119bda-24cf-95a5-26e3-074bea0b03a2>
2011-12-11 14:00:00 +0000

    fix

---  secondstepproject/DistanceLessons/Models/AccountModels.cs
+++  secondstepproject/DistanceLessons/Models/AccountModels.cs
@@ -3,61 +3,25 @@ using System.Web.Mvc;
 
 namespace DistanceLessons.Models
 {
-
-    public class ChangePasswordModel
+    public partial class ChangePasswordModel
     {
-        [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "Поточний пароль")]
         public string OldPassword { get; set; }
-
-        [Required]
-        [StringLength(30, ErrorMessage = "Пароль повинен бути менше {0} і більше ніж {2} символів", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Новий пароль")]
         public string NewPassword { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Підтвердіть новий пароль")]
-        [Compare("NewPassword", ErrorMessage = "Новий пароль і його підтвердження не співпадають!")]
         public string ConfirmPassword { get; set; }
     }
 
-    public class LogOnModel
+    public partial class LogOnModel
     {
-        [Required]
-        [Display(Name = "Логін користувача")]
         public string UserName { get; set; }
-
-        [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "Пароль")]
         public string Password { get; set; }
-
-        [Display(Name = "Запам'ятати?")]
         public bool RememberMe { get; set; }
     }
 
-    public class RegisterModel
+    public partial class RegisterModel
     {
-        [Required]
-        [Display(Name = "Логін користувача")]
         public string UserName { get; set; }
-
-        [Required]
-        [DataType(DataType.EmailAddress)]
-        [Display(Name = "Електронан пошта")]
         public string Email { get; set; }
-
-        [Required]
-        [StringLength(100, ErrorMessage = "Пароль {0} повинен бути менше ніж {2} символів.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Пароль")]
         public string Password { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Підтвердження пароля")]
-        [Compare("Password", ErrorMessage = "Пароль і підтвердження пароля не співпадають")]
         public string ConfirmPassword { get; set; }
     }
 
